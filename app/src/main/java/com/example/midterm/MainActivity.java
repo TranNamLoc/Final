36499902e7946098db1c78362f1e4130f3a3b9a0
TranNamLoc <tranbaophuclhp641999@gmail.com>
2019-06-17 09:36:08 +0700

    17/6

--- app/src/main/java/com/example/midterm/MainActivity.java
+++ app/src/main/java/com/example/midterm/MainActivity.java
@@ -11,23 +11,30 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button facebook;
+    Button google;
     Button signIn;
     Button signUp;
     Typeface typeface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //typeface = Typeface.createFromAsset(getAssets(),"didot.ttf");
+        typeface = Typeface.createFromAsset(getAssets(),"didot.ttf");
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        facebook = (Button) findViewById(R.id.btn_facebook);
+        google = (Button) findViewById(R.id.btn_google);
         signIn = (Button)findViewById(R.id.btn_signin);
         signUp = (Button)findViewById(R.id.btn_signup);
+        facebook.setText("Facebook");
+        google.setText("Google");
+        facebook.setTypeface(typeface);
+        google.setTypeface(typeface);
         signIn.setText("Sign in");
-        //signIn.setTypeface(typeface);
+        signIn.setTypeface(typeface);
         signUp.setText("Sign up");
-        //signUp.setTypeface(typeface);
+        signUp.setTypeface(typeface);
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -36,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
-signIn.setOnClickListener(new View.OnClickListener() {
+        signIn.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
         Toast.makeText(MainActivity.this, "sign in clicked",
--- app/src/main/java/com/example/midterm/SignUp.java
+++ app/src/main/java/com/example/midterm/SignUp.java
@@ -1,24 +1,36 @@
 package com.example.midterm;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
-Button signUp;
-Button facebook;
-Button google;
+    Button facebook;
+    Button google;
+    Button signUp;
+    Typeface typeface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        typeface = Typeface.createFromAsset(getAssets(),"didot.ttf");
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_sign_up);
         facebook = (Button)findViewById(R.id.btn_facebook_signup);
         signUp = (Button)findViewById(R.id.btn_signup_signup);
         google = (Button) findViewById(R.id.btn_google_signup);
         signUp.setText("Sign up");
-        //signUp.setTypeface(typeface);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        signUp.setTypeface(typeface);
+        facebook.setText("Facebook");
+        facebook.setTypeface(typeface);
+        google.setText("Google");
+        google.setTypeface(typeface);
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -27,26 +39,25 @@ Button google;
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SignUp.this, "sign up clicked",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
         facebook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(SignUp.this, "facebook clicked",
+                Toast.makeText(SignUp.this, "Facebook clicked",
                         Toast.LENGTH_LONG).show();
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignUp.this,"Sign up clicked",Toast.LENGTH_LONG).show();
             }
         });
         google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(SignUp.this, "google clicked",
-                        Toast.LENGTH_LONG).show();
+                Toast.makeText(SignUp.this,"Google clicked",Toast.LENGTH_LONG).show();
             }
         });
     }
+
 }
